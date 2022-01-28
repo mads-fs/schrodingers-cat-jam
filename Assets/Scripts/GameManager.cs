ed832b830aa8f7d71b4f6005afa1251b71f467f0
mads <xenon373@gmail.com>
2022-01-28 23:19:56 +0100

    Added a GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,94 @@ namespace SC
 {
     public class GameManager : MonoBehaviour
     {
+        [Min(0), Tooltip("How many times the cat can go to the spirit realm and back. 0 for infinite.")]
+        public int Lives = 0;
 
+        private int _maxLives = 0;
+        private CatController _player;
+
+        #region Singleton
+        public static GameManager Instance;
+
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                Debug.LogWarning($"Only one object of type {GetType()} may exist. Destroying {gameObject}.");
+                Destroy(gameObject);
+            }
+            else
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (Instance == this)
+            {
+                Instance = null;
+            }
+        }
+        #endregion
+
+        private void Start()
+        {
+            _player = FindObjectOfType<CatController>();
+            if (!_player)
+            {
+                Debug.LogError($"No {typeof(CatController)} found. Abort.");
+                Destroy(this);
+            }
+            else
+            {
+                _maxLives = Lives;
+                SubscribeToPlayer();
+            }
+        }
+
+        private void SubscribeToPlayer()
+        {
+            _player.OnAlived += Player_OnAlived;
+            _player.OnUnalived += Player_OnUnAlived;
+        }
+
+        #region OnAlived
+        private void Player_OnAlived(object sender, System.EventArgs e)
+        {
+            Debug.Log("Alived!");
+        }
+        #endregion
+
+        #region OnDied
+        private void Player_OnUnAlived(object sender, System.EventArgs e)
+        {
+            // if MaxLives start at 0 then it can be done infinitely.
+            if (_maxLives != 0)
+            {
+                Lives -= 1;
+                if (Lives == 0)
+                {
+                    GameOver();
+                }
+                else
+                {
+                    TransitionToDeathRealm();
+                }
+            }
+            TransitionToDeathRealm();
+        }
+
+        private void GameOver()
+        {
+            Debug.LogWarning("Game Over!");
+        }
+
+        private void TransitionToDeathRealm()
+        {
+            Debug.Log("Transition To Death Realm.");
+        }
+        #endregion
     }
 }
