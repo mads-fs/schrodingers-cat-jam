70391edf87e38b436ebc373f3decdb5356d7245c
mads <xenon373@gmail.com>
2022-01-29 21:06:36 +0100

    Made a working pass through platform

---
+++ Assets/Scripts/PassthroughPlatform.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SC
+{
+    [RequireComponent(typeof(BoxCollider2D))]
+    public class PassthroughPlatform : MonoBehaviour
+    {
+        private CatController _player;
+        private BoxCollider2D _collider;
+        private void Start()
+        {
+            _player = FindObjectOfType<CatController>();
+            _collider = GetComponent<BoxCollider2D>();
+        }
+
+        private void Update()
+        {
+            if (_player.transform.position.y > transform.position.y)
+            {
+                _collider.enabled = true;
+            }
+            else
+            {
+                _collider.enabled = false;
+            }
+        }
+    }
+}
